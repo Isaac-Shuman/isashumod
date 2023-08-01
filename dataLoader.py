Start search for scaling bug

dataLoader.py
@@ -14,7 +14,7 @@ import torchvision
 annotations_file should be the full path of the csv file with rows [image name, # of spots]
 '''
 
-LAL = 1 #Use 1/LAL of the dataset
+#LAL = 1 #Use 1/LAL of the dataset
 class CustomImageDataset(Dataset):
 
     def __init__(self, annotations_file, path_to_hdf5, transform=None, target_transform=None, test=False):
@@ -25,7 +25,7 @@ class CustomImageDataset(Dataset):
         self.test = test
 
     def __len__(self):
-        return len(self.img_labels)//LAL
+        return len(self.img_labels)#//LAL
 
     def __getitem__(self, idx):
         #get the image as a numpy array
@@ -35,7 +35,7 @@ class CustomImageDataset(Dataset):
          #   idx_offset += 300
         with h5py.File(self.path_to_hdf5, 'r') as f:
             image = f[self.img_labels.iloc[idx + idx_offset, 0]][()]
-            #image = torch.zeros(size=(1, 832, 832), dtype=torch.float32) Used for transform to test having 832*832
+            #image = torch.zeros(size=(1, 1263, 1231), dtype=torch.float32) #Used for transform to test having 832*832
         #get the number of spots
         label = self.img_labels.iloc[idx + idx_offset, 1]
         #label = torch.from_numpy(label)

setupBigTestDataLoad.py
@@ -1,5 +1,7 @@
 #export HDF5_USE_FILE_LOCKING=FALSE
 
+#Initial image size (2527, 2463)
+
 import os
 import csv
 import h5py
@@ -18,7 +20,10 @@ This script creates:
 # 1. Change the directory
 raw_big_dir = '/mnt/tmpdata/data/isashu/exptFileDumps'
 raw_directories = ['1.25ADump', '3.15ADump', '5.45ADump']
-pro_big_dir = '/mnt/tmpdata/data/isashu/smallerLoaders/firstSmallerTestLoaders'
+pro_big_dir = '/mnt/tmpdata/data/isashu/bigLoaders/firstBigTestLoaders'
+
+cond_meth_name = "resize_beta"
+cond_meth = getattr(condition, cond_meth_name)()
 
 #make everything go in the for loop and delete the current pro_dir
 for di in raw_directories: #for direct in raw_directiories
@@ -30,14 +35,11 @@ for di in raw_directories: #for direct in raw_directiories
     cs_filename = os.path.join(pro_dir, "imageNameAndSpots.csv")
     hd = h5py.File(hd_filename, "w-")
 
-    cond_meth_name = "resize_alpha"
-    cond_meth = getattr(condition, cond_meth_name)()
-
     cs = open(cs_filename, 'w')
     writer = csv.writer(cs)
 
     num_files = len(os.listdir(raw_dir))
-    per_files = 0.01
+    per_files = 1
     firstFiles = True #True if you are making the dataset from the first several files as opposed to the last several files
 
     i = 0
@@ -58,7 +60,6 @@ for di in raw_directories: #for direct in raw_directiories
             raw_img = El[0].imageset.get_raw_data(0)[0].as_numpy_array()
 
             cond_img = torch.tensor(raw_img.astype(numpy.float32)).unsqueeze(dim=0)
-            embed()
             cond_img = cond_meth(cond_img)#.squeeze()
             # 5.Store the numpy image in the hdf file
             dset = hd.create_dataset(filename, data=cond_img)

spotFinder.py
@@ -11,8 +11,8 @@ import torch.optim as optim
 from IPython import embed
 from matplotlib import pylab as plt
 from torch.utils.data import random_split
-
 from dataLoader import CustomImageDataset  # import dataloader file
+from torchvision.models import resnet50, vit_b_16,ResNet50_Weights
 
 
 #Setup for the run info files
@@ -53,14 +53,16 @@ def make_dataset(root):
 def make_trainValLoaders(trainset, batch_size):
     # Split the training set into 50% training and 50% validation data
     test_abs = int(len(trainset) * 0.5)
-    generator = torch.Generator().manual_seed(1)
+    generator = torch.Generator().manual_seed(2)
     train_subset, val_subset = random_split(
         trainset, [test_abs, len(trainset) - test_abs], generator=generator
+        #trainset, [144, 144], generator=generator
     )
     trainloader = torch.utils.data.DataLoader(train_subset, batch_size=batch_size,
                                               shuffle=True)
     valloader = torch.utils.data.DataLoader(val_subset, batch_size=batch_size,
                                             shuffle=True)
+
     return trainloader, valloader
 def make_testloader(dataset, batch_size):
     return torch.utils.data.DataLoader(dataset, batch_size=batch_size,
@@ -70,6 +72,9 @@ def make_testloader(dataset, batch_size):
 class Net(nn.Module):
     def __init__(self, mpk=1, fema=3):
         super().__init__()
+
+        #Implement the arch key like you did the preprocessing conditions.
+
         #Images have already gone through a 5*5 MaxPool2d.
         self.pool = nn.MaxPool2d(kernel_size=mpk, stride=mpk)
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=fema, kernel_size=(5, 5), stride=1) #1 input chanels, 3 convolution layers, 5 x 5 convolution
@@ -82,7 +87,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(fema*int(width_of_x*width_of_y), 100)
         self.fc2 = nn.Linear(100, 1)
 
-    def forward(self, x):
+    def forward(self, x, train):
         #x.shape = 505 * 492
         #x = self.pool(x)
         x = F.relu(self.conv1(x))
@@ -96,19 +101,72 @@ class Net(nn.Module):
         #x = self.fc3(x)
         return x
 
-def make_net(usingCuda, useMultipleGPUs, device):
+class Rn(nn.Module):
+    def __init__(self, mpk=1, fema=3):
+        super().__init__()
+
+        self.res = resnet50()
+        #self.tra = vit_b_16(image_size=832, hidden_dim=1)  #patch_size = 16, and 16 *52 = 832
+
+        self.res.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)#nn.Conv2d(1, self.res.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.fc1 = nn.Linear(1000, 1)
+        # self.fc2 = nn.Linear(120, 84)
+
+    def forward(self, x, train):
+        # x.shape = 505 * 492
+        # x = self.pool(x)
+        if train:
+            self.res.train()
+            #self.tra.train()
+        else:
+            self.res.eval()
+            #self.tra.eval()
+
+        x = self.res(x)
+        #x = self.tra(x)
+        x = F.relu(x)
+
+        x = F.relu(self.fc1(x))
+
+        return x
+
+class Tr(nn.Module):
+    def __init__(self, mpk=1, fema=3):
+        super().__init__()
+
+        self.tra = vit_b_16(image_size=832)#, hidden_dim=1)  #patch_size = 16, and 16 *52 = 832
+        self.tra.conv_proj = torch.nn.Conv2d(1, 768, kernel_size=(16, 16), stride=(16, 16))
+
+        self.fc1 = nn.Linear(1000, 1)
+        # self.fc2 = nn.Linear(120, 84)
+
+    def forward(self, x, train):
+        if train:
+            self.tra.train()
+        else:
+            self.tra.eval()
+
+        x = self.tra(x)
+        x = F.relu(x)
+
+        x = F.relu(self.fc1(x))
+
+        return x
+def make_net(device, config):
     # Make the network and have it utilize the gpu
-    net = Net()
+
+    net = config['arc']
     # decide what resources the model will use
-    if (torch.cuda.device_count() > 1) and useMultipleGPUs:
+    if (torch.cuda.device_count() > 1) and config['useMultipleGPUs']:
         print(f"Let's use {torch.cuda.device_count()} GPUs!")
         net = nn.DataParallel(net)
-    if usingCuda:
+    if config['useCuda']:
         net.to(device, dtype=torch.float32)
     return net
 
-def descend(trainloader, usingCuda, device, optimizer, net, criterion, train_losses, etimes):
+def descend(trainloader, useCuda, device, optimizer, net, criterion, train_losses, etimes):
     train_loss = 0
+    train_num_samp = 0
     t= 0
     eb = time.time()
     for i, data in enumerate(trainloader, 0):
@@ -126,13 +184,14 @@ def descend(trainloader, usingCuda, device, optimizer, net, criterion, train_los
         print('took %.4f seconds to zero the gradients' % etime)
 
         ti = time.time()
-        outputs = net(inputs)  # forward
+        outputs = net(inputs, train=True)  # forward
         etime = time.time() - ti
         print('took %.4f seconds to make a prediction' % etime)
 
         ti = time.time()
         loss = criterion(outputs, labels)  # compute the loss
         train_loss += loss.item()
+        train_num_samp += len(labels)
 
         etime = time.time() - ti
         print('took %.4f seconds to compute the loss' % etime)
@@ -146,65 +205,70 @@ def descend(trainloader, usingCuda, device, optimizer, net, criterion, train_los
         optimizer.step()
         etime = time.time() - ti
         print('took %.4f seconds to adjust the parameters' % etime)
-    train_losses.append(train_loss)
+    train_losses.append(train_loss/train_num_samp)
+
     etimes.append(time.time() - eb)
 
 def validate(valloader, device, net, criterion, val_losses):
     val_loss = 0
+    train_num_samp = 0
 
     # ---
     with torch.no_grad():
         for data in valloader:
-            # if usingCuda:
+            # if useCuda:
             #     inputs, labels = data[0].to(device), data[1].to(device)
             # else:
             #     inputs, labels = data
             inputs, labels = data[0].to(device), data[1].to(device)
             # calculate outputs by running images through the network
-            outputs = net(inputs)
-            val_loss += criterion(outputs, labels).item()  # compute the loss
+            outputs = net(inputs, train=False)
+
+            loss_as_ten = criterion(outputs, labels)
+            val_loss += loss_as_ten.item()  # compute the loss
+            train_num_samp += len(labels)
     # ----
-    val_losses.append(val_loss)
-    embed()
+    #embed()
+    val_losses.append(val_loss/train_num_samp)
+
 
 def save_epoch(net, epoch, folderName):
     pathToModel = os.path.join(folderName, 'modelE'+str(epoch)+'.pth')
     torch.save(net.state_dict(), pathToModel)
 
-def train_up_model(useCuda, useMultipleGPUs, device, trainloader, valloader, etimes, train_losses, val_losses, epochs, folderName):
+def train_up_model(device, trainloader, valloader, etimes, train_losses, val_losses, folderName, config):
     #Make the network and have it utilize the gpu
-    net = make_net(useCuda, useMultipleGPUs, device)
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(net.parameters(), lr=1e-10, momentum=0.3) #lr=0.00000000001, momentum=0.3
+    net = make_net( device=device, config=config)
+    criterion = nn.MSELoss(reduction='mean')
+    optimizer = optim.SGD(net.parameters(), lr=config['lr'], momentum=config['mom']) #lr=0.00000000001, momentum=0.3
 
 
-    for epoch in range(epochs):  # loop over the dataset multiple times
+    for epoch in range(config['epochs']):  # loop over the dataset multiple times
         validate(valloader=valloader, device=device, net=net, criterion=criterion, val_losses=val_losses)
-        descend(trainloader=trainloader, usingCuda=useCuda, device=device, optimizer=optimizer, net=net,
+        descend(trainloader=trainloader, useCuda=config['useCuda'], device=device, optimizer=optimizer, net=net,
                 criterion=criterion, train_losses=train_losses, etimes=etimes)
-        save_epoch(net=net, epoch=epoch, folderName=folderName)
-
+        #save_epoch(net=net, epoch=epoch, folderName=folderName)
 
     print('Finished Training')
     return net
 
-def test(testloader, useCuda, device, net, err_margin):
+def test(testloader, device, net, config):
     # Test on the whole dataset
     correct = 0
     total = 0
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in testloader:
-            if useCuda:
+            if config['useCuda']:
                 inputs, labels = data[0].to(device), data[1].to(device)
             else:
                 inputs, labels = data
-            outputs = net(inputs)
+            outputs = net(inputs, train=False)
             print('arrived at predictions')
             total += labels.size(0)
-            correct += (abs(outputs - labels) / labels < err_margin).sum().item()
+            correct += (abs(outputs - labels) / labels < config['err_margin']).sum().item()
     return correct/total
-def save_run(net, train_losses, etimes, val_losses, accuracies, rootForTrain, rootForTest, folderName):
+def save_run(net, train_losses, etimes, val_losses, accuracies, rootForTrain, rootForTest, folderName, config):
     #Log the statistics of the run and save the model
     log_filename = os.path.join(folderName, 'metrics.log')
     pathToModel = os.path.join(folderName, 'modelFinal.pth')
@@ -212,6 +276,7 @@ def save_run(net, train_losses, etimes, val_losses, accuracies, rootForTrain, ro
 
     logging.basicConfig(filename=log_filename, encoding='utf-8', level=logging.INFO, filemode='a')
     logging.info(f'Network is: {net}')
+    logging.info(f'Config is: {config}')
     logging.info(f'rootForTrain: {rootForTrain}')
     logging.info(f'rootForTest: {rootForTest}')
     logging.info(f'Epoch run times: {etimes}')
@@ -244,12 +309,17 @@ def loadModel(pathToModel):
 def main():
 
     t = time.time()
-    err_margin = 0.1  # How far off a "correct" prediction can be as a percentage (0.1 is 10%)
-    batch_size = 10
-    epochs = 5
-    useMultipleGPUs = False
-    useCuda = True
-    lm = False #Do you wanna load a model?
+    config = {
+        "err_margin": 0.1,
+        "batch_size": 1,
+        "epochs": 10,
+        "useMultipleGPUs": False,
+        "useCuda": True,
+        "lm": False,
+        "arc": Net(),
+        "lr":  1e-5,
+        "mom": 0.1
+    }
 
     rootForTrain = "/mnt/tmpdata/data/isashu/smallerLoaders/firstSmallerTrainLoader"
     rootsForTest = ["/mnt/tmpdata/data/isashu/smallerLoaders/firstSmallerTestLoaders/1.25ALoader",
@@ -261,44 +331,41 @@ def main():
     # use the gpu
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    trainloader, valloader = make_trainValLoaders(make_dataset(root=rootForTrain), batch_size=batch_size)
+    trainloader, valloader = make_trainValLoaders(make_dataset(root=rootForTrain), batch_size=config['batch_size'])
     testloaders = []
     for rootForTest in rootsForTest:
-        testloaders.append(make_testloader(make_dataset(root=rootForTest), batch_size=batch_size))
+        testloaders.append(make_testloader(make_dataset(root=rootForTest), batch_size=config['batch_size']))
+    testloaders= [] #Temp
 
     etimes = []
     train_losses = []
     val_losses = []
-    if not lm:
+    if not config['lm']:
         net = train_up_model(
-            useCuda=useCuda,
-            useMultipleGPUs=useMultipleGPUs,
             device=device,
             trainloader=trainloader,
             valloader=valloader,
             etimes=etimes,
             train_losses=train_losses,
             val_losses=val_losses,
-            epochs=epochs,
             folderName=folderName,
             config=config
         )
     else:
         net = loadModel("/mnt/tmpdata/data/isashu/runFolders/run_2023-08-STUFF.pth") #Current network must match loaded network
-        if (torch.cuda.device_count() > 1) and useMultipleGPUs:
+        if (torch.cuda.device_count() > 1) and config['useMultipleGPUS']:
             print(f"Let's use {torch.cuda.device_count()} GPUs!")
             net = nn.DataParallel(net)
-        if useCuda:
+        if config['useCuda']:
             net.to(device, dtype=torch.float32)
 
     accuracies = []
     for testloader in testloaders:
         accuracy = test(
             testloader=testloader,
-            useCuda=useCuda,
             device=device,
             net=net,
-            err_margin=err_margin
+            config=config
         )
         accuracies.append(accuracy)
 